b4288b99d7ce0da6c79439ae3121a24423f11229
Mavricus <mamakc61@gmail.com>
2020-10-01 00:49:30 +0300

    Improvement
    - Fixed type of cache.get

--- src/CacheController.ts
+++ src/CacheController.ts
@@ -1,5 +1,5 @@
 export interface ICacheController {
-    get<T>(name: string): Promise<T>;
+    get<T>(name: string): Promise<T | null>;
     set<T>(name: string, data: T, ttl: number): Promise<void>;
     delete<T>(name: string): Promise<void>;
 }
